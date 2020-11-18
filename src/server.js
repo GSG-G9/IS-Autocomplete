190b0fddd18214c18b35e86c18c908fe737ce4ee
Israa91 <eng.israa.1991@gmail.com>
2020-11-18 20:57:50 +0200

    fix errors

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const router = require('./router');
 
 const server = http.createServer(router);
-require('env2')('.env');
+// require('env2')('.env');
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`The server is run on http://localhost:${port}`)
