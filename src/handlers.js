bda980d89a656dc1e85fd963487d75f0de73a81f
Sondos-Alalami <sondos.alalami@gmail.com>
2020-11-19 10:50:50 +0200

    adding search function and its test Realates #4

--- src/handlers.js
+++ src/handlers.js
@@ -1,46 +1,44 @@
 const path = require('path');
 const fs = require('fs');
- require("http");
+require('http');
 
 const handleMain = (response) => {
-    console.log("handleMain")
-    const filePath = path.join(__dirname, '..', 'public', 'index.html');
-    fs.readFile(filePath, (error, file) => {
-        if (error) {
-            response.writeHead(500, { 'Content-Type': 'text/html' });
-            response.end('<h1> error with server </h1>')
-        } else {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end(file);
-        }
-
-    })
-
-}
+  console.log('handleMain');
+  const filePath = path.join(__dirname, '..', 'public', 'index.html');
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      response.writeHead(500, { 'Content-Type': 'text/html' });
+      response.end('<h1> error with server </h1>');
+    } else {
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.end(file);
+    }
+  });
+};
 
 const handlePublic = (response, url) => {
-    console.log("handleMain")
-    const ext = path.extname(url);
-    const mimiTypes = {
-      ".css": "text/css",
-      ".js": "text/javascript",
-      ".jpg": "image/jpeg",
-      '.png': 'image/png',
-      '.ico' :'image/x-icon',
-      '.html': "text/html",
-      '.js' :"text/javascript"
-    };
-  
-    const filepath = path.join(__dirname, "..",'public', url);
-    fs.readFile(filepath, (error, file) => {
-      if (error) {
-        response.writeHead(500, { "Content-Type": "text/html" });
-        response.end("server error");
-      } else {
-        response.writeHead(200, { "Content-Type": mimiTypes[ext] });
-        response.end(file);
-      }
-    });
+  console.log('handleMain');
+  const ext = path.extname(url);
+  const mimiTypes = {
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.jpg': 'image/jpeg',
+    '.png': 'image/png',
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
   };
-  
-module.exports = { handleMain , handlePublic};
+
+  const filepath = path.join(__dirname, '..', 'public', url);
+  fs.readFile(filepath, (error, file) => {
+    if (error) {
+      response.writeHead(500, { 'Content-Type': 'text/html' });
+      response.end('server error');
+    } else {
+      response.writeHead(200, { 'Content-Type': mimiTypes[ext] });
+      response.end(file);
+    }
+  });
+};
+
+module.exports = { handleMain, handlePublic };
---
+++ test/test.js
@@ -0,0 +1,17 @@
+const search = require('../public/script.js');
+
+const countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central Arfrican Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cuba', 'Curacao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt'];
+
+describe('Testing search', () => {
+  test('Should return Albania, Algeria when entered al ', () => {
+    const actual = search('al', countries);
+    const expected = ['Albania', 'Algeria'];
+    expect(actual).toEqual(expected);
+  });
+
+  test('Should return Belarus Belgium Belize when entered bel', () => {
+    const actual = search('bel', countries);
+    const expected = ['Belarus', 'Belgium', 'Belize'];
+    expect(actual).toEqual(expected);
+  });
+});
